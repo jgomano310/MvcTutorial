50503527255e8bb32b06fc724a0753d7959c1667
DESKTOP-FO3JRPK\javier <javiergom310@gmail.com>
2022-11-14 11:35:19 +0100

    Agregar archivos de proyecto.

---
+++ MvcTutorial/Controllers/UsuariosController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class UsuariosController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListaUsuarios()
+        {
+
+            List<string> ListaUsuarios=new List<string>();
+            ListaUsuarios.Add("javi");
+            ListaUsuarios.Add("gómez");
+            ListaUsuarios.Add("cano");
+            ListaUsuarios.Add("ramirez");
+            return View(ListaUsuarios);
+        }
+    }
+}
